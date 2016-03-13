75677abb882656a0176a66fb31e50e1c7e79e9ea
Salim Ben Khaled <salimbenkhaled93@gmail.com>
2016-03-13 13:27:48 -0600

    Adding table classes

---
+++ people.java
@@ -0,0 +1,16 @@
+public class People{
+
+	String sin;
+	String name;
+	float height;
+	float weight;
+	String eyecolor;
+	String haircolor;
+	String addr;
+	String gender;
+	String date;
+	public People(String sin, String name){
+		this.sin = sin;
+		this.name = name;
+	}
+}
