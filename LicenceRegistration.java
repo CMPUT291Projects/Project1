fd05f2fe47b017a772cdfe9bd9831ea9db6e2f82
Salim Ben Khaled <salimbenkhaled93@gmail.com>
2016-03-15 12:39:41 -0600

    FIxed date issue in licence registration

--- LicenceRegistration.java
+++ LicenceRegistration.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 import java.sql.SQLException;
 import javax.sql.rowset.serial.SerialException;
 import java.text.ParseException;
+import java.lang.NullPointerException;
 
 
 public class LicenceRegistration
@@ -18,17 +19,27 @@ public class LicenceRegistration
 	}
 
 	public void run() {
-		System.out.print("licence no\n");
+		System.out.print("Insert licence no.\n");
 		Console co = System.console();
 		String licence_no = co.readLine();
 
-		System.out.print("sin\n");
-		String sin = co.readLine();
+		String sin;
+		String temp_sin;
+		do {
+			System.out.print("Insert SIN.\n");
+			temp_sin = co.readLine();
+			if(!idExists(temp_sin, conn)) {
+				sin = temp_sin;
+			} else {
+				System.out.print("SIN already exists, try again\n");
+			}
+		} while (serial_no == null);
 
-		System.out.print("classType\n");
+		System.out.print("Insert classType.\n");
 		String classType = co.readLine();
 
-		String file1 = "window-sm.jpg";
+		System.out.print("Reading in picture.\n");
+		String file1 = "test.jpg";
 		File file = new File(file1);
 		FileInputStream fileInput = null;
 		try{
@@ -50,36 +61,45 @@ public class LicenceRegistration
 		catch(IOException e){
 			System.out.print("File error.");
 		}
+		catch(NullPointerException e){
+			System.out.print("Null pointer. Used the wrong file.");
+		}
 
 		SerialBlob picture = null;
 		try{
 			picture = new SerialBlob(buffer.toByteArray());
 		}
 		catch(SerialException e){
-			System.out.print("Error with Byte array");
+			System.out.print("Error with Byte array.");
 		}
 		catch(SQLException e){
-			System.out.print("Error with Serialblob");
+			System.out.print("Error with Serialblob.");
 		}
 
-		System.out.print("issuing_date\n");
-		String issuing_String = co.readLine();
+		goodValue = false;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		java.util.Date issuing_date = null;
-		try{
-			issuing_date = new SimpleDateFormat().parse(issuing_String);
-		}
-		catch(ParseException e){
-			System.out.print("Error reading issuing date.");
+		while (!goodValue) {
+			System.out.print("Insert issuing date (yyyy-MM-dd).\n");
+			try {
+				birthday = format.parse(co.readLine());
+				goodValue = true;
+			} catch (Exception ex) {
+				System.out.print("invalid issuing date format, use 'yyyy-MM-dd'\n");
+			}
 		}
 
-		System.out.print("expiring_date\n");
-		String expiring_String = co.readLine();
+		goodValue = false;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		java.util.Date expiring_date = null;
-		try{
-			expiring_date = new SimpleDateFormat().parse(expiring_String);
-		}
-		catch(ParseException e3){
-			System.out.print("Error reading expiring date.");
+		while (!goodValue) {
+			System.out.print("Insert expiring date (yyyy-MM-dd).\n");
+			try {
+				birthday = format.parse(co.readLine());
+				goodValue = true;
+			} catch (Exception ex) {
+				System.out.print("invalid expiration date format, use 'yyyy-MM-dd'\n");
+			}
 		}
 
 		drive_licence dl = new drive_licence(licence_no, sin, classType, picture, issuing_date, expiring_date);
